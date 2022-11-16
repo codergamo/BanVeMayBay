9a4e23fb9ea072e27d9e33143c304e49d684de33
codergamo <90610860+codergamo@users.noreply.github.com>
2022-11-16 20:06:05 +0700

    Update PhanQuyen.sql

--- Database_SQL/complete/PhanQuyen.sql
+++ Database_SQL/complete/PhanQuyen.sql
@@ -1,14 +1,14 @@
-﻿create proc PHANQUYEN 
-@Username char(20)
-as 
-begin
-	declare @role char(20)
-	set @role = (select PHANLOAI from TAIKHOAN where TenTaiKhoan = @Username)
-
-	if @role = '1'
-		print N'Bạn có quyền Admin'
-	else print N'Bạn có quyền Employee'
-end
+﻿--create proc PHANQUYEN 
+--@Username char(20)
+--as 
+--begin
+--	declare @role char(20)
+--	set @role = (select PHANLOAI from TAIKHOAN where TenTaiKhoan = @Username)
+--
+--	if @role = '1'
+--		print N'Bạn có quyền Admin'
+--	else print N'Bạn có quyền Employee'
+--end
 
 create function Dang_Nhap(@Username char(20), @Password char(20))
 returns table 
